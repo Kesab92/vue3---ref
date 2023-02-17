7de34025c658c0888348aace2881cd21f86578de
Kesab92 <107046585+Kesab92@users.noreply.github.com>
2023-02-17 14:46:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue3---ref
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-o2f83m)
---
+++ src/components/Form.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="s-Form">
+    <p class="s-Message">{{ message }}</p>
+    <input
+      class="s-Message__holder"
+      type="text"
+      placeholder="type something..."
+      v-model="message"
+    />
+    <button class="s-Button" @click="onSubmit">submit</button>
+  </div>
+</template>
+<script>
+import { ref, reactive } from 'vue';
+export default {
+  setup() {
+    const message = ref('');
+    const onSubmit = () => {
+      console.log(`The typed message is${message.value}`);
+    };
+    return {
+      message,
+      onSubmit,
+    };
+  },
+};
+</script>
+<style>
+.s-Form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+.s-Message {
+  color: green;
+}
+.s-Message__holder {
+  height: 40px;
+}
+.s-Button {
+  color: white;
+  width: 100px;
+  background-color: green;
+}
+</style>
